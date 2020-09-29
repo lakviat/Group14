5131ff86599585fe157bfaac7a365e9873806967
ZDagasan <zedasa80@gmail.com>
2020-09-28 20:11:11 -0500

    I am adding changes from the master branch

--- src/Zeliha.java
+++ src/Zeliha.java
@@ -2,10 +2,6 @@ public class Zeliha {
     public static void main(String[] args) {
 
         System.out.println("Helloooo Group 14!");
-        System.out.println("new Line");
-        System.out.println("very new");
-        System.out.println("very soon");
-
 
     }
 }
