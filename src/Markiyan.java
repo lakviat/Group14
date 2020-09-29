cb6cb37eedb30e9f2239e93c63843ccb9d6021f4
markiyansav <markiyansav@gmail,com>
2020-09-28 20:37:16 -0500

    Mark

--- src/Markiyan.java
+++ src/Markiyan.java
@@ -2,5 +2,6 @@ public class Markiyan {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("TEST");
+        System.out.println("Test2");
     }
 }
