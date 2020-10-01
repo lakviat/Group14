a351605c7bbd692d5b3b829a0a31637348d79a3f
ZDagasan <zedasa80@gmail.com>
2020-10-01 16:29:41 -0500

    changes

--- src/Zeliha.java
+++ src/Zeliha.java
@@ -2,6 +2,8 @@ public class Zeliha {
     public static void main(String[] args) {
 
         System.out.println("Helloooo Group 14!");
+        System.out.println("add your code with no conflict");
+        System.out.println("This is a smaple for group study");
 
     }
 }
