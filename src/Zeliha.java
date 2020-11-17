15a74f4f9f06758bf76feb29fd5fb6bb319b2a72
ZDagasan <zedasa80@gmail.com>
2020-11-16 18:49:01 -0600

    second conflict from Zeliha

--- src/Zeliha.java
+++ src/Zeliha.java
@@ -3,6 +3,10 @@ public class Zeliha {
 
         System.out.println("Helloooo Group 14!");
         System.out.println(" I am ready to create a conflict");
+        System.out.println("This was not enough one more trial");
+
+
+
 
     }
 }
