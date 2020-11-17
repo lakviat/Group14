672991353c6af5fa0917db49e32afd6122ab3c90
lakviat <lakviat@yahoo.com>
2020-11-16 19:44:11 -0600

    adding jenkinsfile to the Barch 20

--- src/EverybodyWorkshere.java
+++ src/EverybodyWorkshere.java
@@ -1,15 +1,15 @@
 public class EverybodyWorkshere {
     public static void main(String[] args) {
         System.out.println("Im adding this from my site Nurlan ");
-
+        Duplicates();
     }
-    public void Duplicates(){
+    public static void Duplicates(){
         int [] num = {10,20,30,40,50,50};
         for (int i = 0; i <num.length ; i++) {
             boolean duplicates = false;
             for (int j = 0; j <num.length ; j++) {
-                if(i == j && num[i] != num[j]){
-                    duplicates = false;
+                if(i != j && num[i] == num[j]){
+                    duplicates = true;
                     break;
                 }
             }if(duplicates == true){
