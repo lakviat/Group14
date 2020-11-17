e8237c0d293b6fe8064f891a2fc1c728c4fe3ec9
markiyansav <68977172+markiyansav@users.noreply.github.com>
2020-11-16 20:27:13 -0600

    test

--- src/Markiyan.java
+++ src/Markiyan.java
@@ -1,11 +1,22 @@
 public class Markiyan {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("TEST");
-        System.err.println("Test2");
-        /**
-         * Test 1
-         * Test2
-         */
+
+
+
+
+        public static void main(String[] args) {
+            int [] num = {10,20,30,40,50,60};
+            // print out rist and last iteratvely
+            // OUTPUT WILL BE 10 60 20 50 30 40
+            // make sure the code i dynamic and can handle any size of the array
+            // PUT YOUR CODE HERE -- > Good Luck
+            String result=" ";
+            int j=num.length-1;
+            for (int i=0;i<j;i++){
+                result+=num[i]+""+num[j]+" ";
+                j--;
+
+            }
+            System.out.println(result);
+        }
     }
-}
+
