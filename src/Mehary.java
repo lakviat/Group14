f9629e2ea197d169f2eeedc18bbbc1afefa41657
Mehary16 <mehary.aynealem@gmail.com>
2020-11-16 18:49:58 -0600

    Hello group14

--- src/Mehary.java
+++ src/Mehary.java
@@ -2,6 +2,8 @@ public class Mehary {
     public static void main(String[] args) {
 
         System.out.println(" Hello group 14 people ");
-        System.out.println("This is so interesting class, we need to master this with the of these cools guys");
+        System.out.println("This is so interesting class, we need to master this " +
+                "with the of these cools guys");
+        System.out.println(" git and git hub is always fun and challenging");
     }
 }
