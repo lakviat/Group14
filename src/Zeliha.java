51cd3e52f8eec96f1151677b5648fd5ff0dc8b53
lakviat <lakviat@yahoo.com>
2020-11-16 18:41:18 -0600

    adding jenkinsfile to the Barch 20

--- src/Zeliha.java
+++ src/Zeliha.java
@@ -6,6 +6,6 @@ public class Zeliha {
         // I was working on this branch for 3 days
         // but i didn't know if anyone added to master
         // i completed my feature i need to push to master
-
+        // this is example with not resolved highlightes
     }
 }
