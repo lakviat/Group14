915b2956cf12e2e1b40ef0bd4125b5f2a325433e
Mehary16 <mehary.aynealem@gmail.com>
2020-11-16 18:47:12 -0600

    Group14 hello

--- src/Mehary.java
+++ src/Mehary.java
@@ -1,5 +1,7 @@
 public class Mehary {
     public static void main(String[] args) {
+
         System.out.println(" Hello group 14 people ");
+        System.out.println("This is so interesting class, we need to master this with the of these cools guys");
     }
 }
