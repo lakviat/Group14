aef9065d56b0472c47e96bf77b492f6d29d2e7ab
lakviat <lakviat@yahoo.com>
2020-11-16 18:39:06 -0600

    adding jenkinsfile to the Barch 20

--- src/Zeliha.java
+++ src/Zeliha.java
@@ -3,5 +3,9 @@ public class Zeliha {
 
         System.out.println("Helloooo Group 14!");
 
+        // I was working on this branch for 3 days
+        // but i didn't know if anyone added to master
+        // i completed my feature i need to push to master
+
     }
 }
